2c32e2733e149ed52ec5b714bb071f610dc060f6
unknown <swarudi555@gmail.com>
2023-11-11 22:35:52 +0530

    first commit

---
+++ credentials.js
@@ -0,0 +1,8 @@
+const CLIENT_ID =
+  "208162011483-ounrqck2g2oa9saf1l160hikuuv5lqdd.apps.googleusercontent.com";
+const CLEINT_SECRET = "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";
+const REDIRECT_URI =
+  "4/0AfJohXnwOxHWrwnEOGkckQpzmeXqvhD7ami1oXUuwpI89BBR8Czg53hXTWY5l12VhWIiSg";
+const REFRESH_TOKEN =
+  "1//04HMmZtEmPs4ACgYIARAAGAQSNwF-L9IrFWV4LxmGvPDMSHqSJQxc7nKAriufQ6S7mL-RAMUzQTdTqJQtyJnRWtHPAhvsiqQGInI";
+module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
